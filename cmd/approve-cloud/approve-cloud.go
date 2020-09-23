de2e957c35cdefd276823698cc1405a8373cb3a4
Thomas Habets <habets@google.com>
2020-09-23 22:20:06 +0200

    approve-cloud: Add initial working (but insecure) server side

--- cmd/approve-cloud/approve-cloud.go
+++ cmd/approve-cloud/approve-cloud.go
@@ -18,8 +18,13 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
@@ -34,8 +39,21 @@ var (
 	cloud   = flag.String("cloud", "https://europe-west2-simapprover.cloudfunctions.net/", "Cloud base URL.")
 )
 
-func send(ctx context.Context, req []byte) error {
-	r, err := http.NewRequestWithContext(ctx, "POST", *cloud+"ask", bytes.NewBuffer(req))
+func send(ctx context.Context, b []byte) error {
+	type Req struct {
+		Device  string `json:"device"`
+		Content []byte
+	}
+	req := Req{
+		Device:  *device,
+		Content: b,
+	}
+	reqbs, err := json.Marshal(&req)
+	if err != nil {
+		return err
+	}
+
+	r, err := http.NewRequestWithContext(ctx, "POST", *cloud+"request", bytes.NewBuffer(reqbs))
 	if err != nil {
 		return err
 	}
@@ -44,15 +62,63 @@ func send(ctx context.Context, req []byte) error {
 	if err != nil {
 		return err
 	}
-	_ = resp
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code non-200: %d", resp.StatusCode)
+	}
+	log.Infof("Supposedly succussfully sent FCM message to %q", *cloud+"request")
 	return nil
 }
 
+func poll(ctx context.Context, id string) error {
+	type PollReq struct {
+		ID string `json:"id"`
+	}
+	req := PollReq{
+		ID: id,
+	}
+	reqb, err := json.Marshal(&req)
+	if err != nil {
+		return err
+	}
+	r, err := http.NewRequestWithContext(ctx, "POST", *cloud+"poll-reply", bytes.NewBuffer(reqb))
+	if err != nil {
+		return err
+	}
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	dat, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code non-200: %d", resp.StatusCode)
+	}
+	if len(dat) == 0 {
+		return errors.New("not there")
+	}
+	return sim.Reply(path.Join(*sockDir, req.ID), dat)
+}
+func poll_loop(ctx context.Context, id string) {
+	// TODO: exponential backoff.
+	for {
+		if err := poll(ctx, id); err != nil {
+			log.Errorf("Poll error id %q: %v", id, err)
+			time.Sleep(2 * time.Second)
+			continue
+		}
+		log.Infof("Got reply for %q", id)
+		return
+	}
+}
 func main() {
 	flag.Parse()
 	watcher := sim.NewWatcher(*sockDir)
 	go watcher.Run()
 	ctx := context.Background()
+
+	log.Infof("Running…")
 	for {
 		func() {
 			cancel, ch := watcher.Add()
@@ -75,6 +141,7 @@ func main() {
 					log.Errorf("Failed to read request: %v", err)
 					break
 				}
+				go poll_loop(ctx, path.Base(event.Name))
 
 				// Send it.
 				if err := send(ctx, req); err != nil {
