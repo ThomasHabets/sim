573804862173bbc8238b9f3a8c6ba74adbbdc301
Thomas Habets <thomas@habets.se>
2024-01-05 19:08:55 +0000

    app: New icon

---
+++ app/SimApprover/app/src/main/res/mkicons.sh
@@ -0,0 +1,5 @@
+convert icon.png -resize 48x48 mipmap-mdpi/icon.png
+convert icon.png -resize 72x72 mipmap-hdpi/icon.png
+convert icon.png -resize 96x96 mipmap-xhdpi/icon.png
+convert icon.png -resize 144x144 mipmap-xxhdpi/icon.png
+convert icon.png -resize 192x192 mipmap-xxxhdpi/icon.png
